8fc598822400b0d8b34ae7d596f1f33a01daa170
chelseaY119 <ayang106@my.bcit.ca>
2024-09-10 11:38:55 -0700

    initial commitment

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// write the
+class WriteMessage {
+  constructor(content) {
+    this.content = content;
+  }
+
+  getContent() {
+    return this.content;
+  }
+
+  addMessage() {}
+
+  removeMessage() {}
+
+  modifyMessage() {}
+}
